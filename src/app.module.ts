1f389c9dba1716cf9bf96a9fa90218d025f7284f
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-08-07 18:46:29 +0100

    initial commit for books resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BooksModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'restapi',
+      entities: ['src/**/**.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    credentials: true,
+    origin: function (origin, callback) {
+      console.log(`Origin ${origin} is being granted CORS access`);
+      callback(null, true);
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
