a032963b23e4334c5dda3db5b510517c1e7ad0bb
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-08-07 21:07:05 +0100

    add declaration for create book dto

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,11 @@
-export class CreateBookDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  author: string;
+}
