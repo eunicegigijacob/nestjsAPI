ecc1dc26a1b726d6f0ae406475a5ff52640740b3
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-08-07 21:07:50 +0100

    add declaration for book entity

--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,1 +1,13 @@
-export class Book {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  author: string;
+}
